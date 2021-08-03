4b57cdcb1499d28bd83c65b5e7fd4591f42935db
xdrie <xdrie@alt.icu>
2021-08-03 14:01:53 -0700

    add hello world script

--- demo/source/app.d
+++ demo/source/app.d
@@ -1,7 +1,14 @@
 import std.stdio;
+import std.file;
+import std.path;
+import std.utf;
 import wren;
 
-void main() {
+public static char* c_str(string str) {
+	return str.toUTFz!(char*)();
+}
+
+void main(string[] args) {
 	// create wren config
 	WrenConfiguration config;
 	wrenInitConfiguration(&config);
@@ -10,9 +17,16 @@ void main() {
 	// create vm
 	WrenVM* vm = wrenNewVM(&config);
 
-	// execute print code
-	WrenInterpretResult result = wrenInterpret(vm, "my_module",
-			"System.print(\"I am running in a VM!\")");
+	if (args.length <= 1) {
+		// execute print code
+		WrenInterpretResult result = wrenInterpret(vm, "my_module",
+				"System.print(\"I am running in a VM!\")");
+	} else {
+		// load file
+		auto script_file = args[1];
+		auto file_contents = std.file.readText(script_file);
+		WrenInterpretResult result = wrenInterpret(vm, dirName(script_file).c_str, file_contents.c_str);
+	}
 }
 
 extern (C) void writeFn(WrenVM* vm, const char* text) {
