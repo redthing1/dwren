6344141c84b8bbe55869a50e9dad9fe9911328da
xdrie <xdrie@alt.icu>
2021-08-03 14:19:50 -0700

    show errors

--- demo/source/app.d
+++ demo/source/app.d
@@ -17,16 +17,37 @@ void main(string[] args) {
 	// create vm
 	WrenVM* vm = wrenNewVM(&config);
 
+	string module_name;
+	string script_source;
 	if (args.length <= 1) {
-		// execute print code
-		WrenInterpretResult result = wrenInterpret(vm, "my_module",
-				"System.print(\"I am running in a VM!\")");
+		// basic print test
+		module_name = "test_module";
+		script_source = "System.print(\"I am running in a VM!\")";
 	} else {
 		// load file
 		auto script_file = args[1];
-		auto file_contents = std.file.readText(script_file);
-		WrenInterpretResult result = wrenInterpret(vm, dirName(script_file).c_str, file_contents.c_str);
+		immutable auto file_contents = std.file.readText(script_file);
+		module_name = dirName(script_file);
+		script_source = file_contents;
 	}
+	immutable WrenInterpretResult result = wrenInterpret(vm, module_name.c_str, script_source.c_str);
+	switch (result) {
+	case WrenInterpretResult.WREN_RESULT_COMPILE_ERROR: {
+			printf("compile error!\n");
+		}
+		break;
+	case WrenInterpretResult.WREN_RESULT_RUNTIME_ERROR: {
+			printf("runtime error!\n");
+		}
+		break;
+	case WrenInterpretResult.WREN_RESULT_SUCCESS: {
+			printf("success!\n");
+		}
+		break;
+		default: break;
+	}
+
+	wrenFreeVM(vm);
 }
 
 extern (C) void writeFn(WrenVM* vm, const char* text) {
