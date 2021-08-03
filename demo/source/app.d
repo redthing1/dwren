3bd6c9bd54731737046ffa5de404368b78ec690b
xdrie <xdrie@alt.icu>
2021-08-03 13:53:04 -0700

    add basic working demo

--- demo/source/app.d
+++ demo/source/app.d
@@ -1,6 +1,20 @@
 import std.stdio;
+import wren;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+void main() {
+	// create wren config
+	WrenConfiguration config;
+	wrenInitConfiguration(&config);
+	config.writeFn = &writeFn; // set write
+
+	// create vm
+	WrenVM* vm = wrenNewVM(&config);
+
+	// execute print code
+	WrenInterpretResult result = wrenInterpret(vm, "my_module",
+			"System.print(\"I am running in a VM!\")");
+}
+
+extern (C) void writeFn(WrenVM* vm, const char* text) {
+	printf("%s", text);
 }
